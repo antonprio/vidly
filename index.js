7ba4e7e52c91d71158274d6cf0f1f6eeda970a6f
Anton Prio Hutomo <antonpriohutomo@gmail.com>
2021-01-11 20:22:47 +0700

    adding error middleware

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const error = require('./middleware/error');
 const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -27,6 +28,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
